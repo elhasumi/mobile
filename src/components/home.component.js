74485c9b464c59fcdc18f802327ef18348c8456b
Mohammed Elhasumi <elhasumi88@gmail.com>
2021-02-18 05:41:09 -0800

    mobile

---
+++ src/components/home.component.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import React, { Component } from 'react';
+export default class Home extends Component {
+
+  state = {};
+  componentDidMount() {
+    
+    axios.get('user').then(
+      res => {
+        this.setState({
+          user: res.data
+        });
+      },
+      err => {
+        console.log(err)
+      }
+    )
+  }
+  render() {
+    if(this.state.user){
+      return(
+      <h2>Hi {this.state.user.first_name} {this.state.user.last_name}</h2>
+      )
+    }
+    return (
+        <h2></h2>
+    );
+  }
+}
---
+++ src/components/login.component.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+import React, { Component } from 'react';
+export default class Login extends Component {
+
+  handleSubmit = e => {
+    e.preventDeafult();
+  const data = {
+    email: this.email,
+    password: this.passowrd
+  }
+  axios.post('login', data)
+  .then(res => {
+    localStorage.setItem('token', res.data.token);
+  })
+  .catch(err => {
+   console.log(err);
+    
+  })
+};
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+      <h3>Login</h3>
+      
+      <div className="form-group">
+       <label>Email</label>
+       <input type="email" className="form-control" placeholder="Email" 
+       onChange={e => this.email = e.target.value}/>
+      </div>
+
+      <div className="form-group">
+       <label>Password</label>
+       <input type="password" className="form-control" placeholder="Password"
+       onChange={e => this.passowrd = e.target.value} />
+      </div>
+
+      
+      <button className="btn btn-primary btn-block">Login</button>
+  </form>
+    );
+  }
+}
